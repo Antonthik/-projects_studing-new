df8e8cc52f7e6dfb6d3ba83c2406d13d40cc03da
Antonthik <93084395+Antonthik@users.noreply.github.com>
2021-11-09 22:45:05 +0300

    Add files via upload

---
+++ Lesson4-task1/Lesson4-task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lesson4_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = arrayFIO(); // формируем входной массив
+            splitFIO(arr); // вывод общей строки
+
+        }
+        //Функция формирующая общую строку ФИО и вывод на дисплей
+        static void GetFullName(string firstName, string lastName, string patronymic)
+        {
+            string fullName = ($"{firstName} {lastName} {patronymic}");            
+            Console.WriteLine(fullName);
+        }
+
+        //Функция формирования входных данных
+        static String[] arrayFIO (){
+            String[] arr = new string[] { 
+            "Иванов Иван Иванович",
+            "Сидоров Петр Сергеевич",
+            "Петрова Татьяна Сергеевна"};
+            return arr;
+            }
+
+        //Функция разбиения строки на ФИО и передача функцию вывода на дисплей
+        static void splitFIO( string[] arr)
+        {
+            for(int i=0;i< arr.Length; i++)
+            {
+                if (arr[i]!="") {
+                    string[] arrSplit = arr[i].Split(" ");
+                    if (arrSplit.Length == 3){
+                        GetFullName(arrSplit[0], arrSplit[1], arrSplit[2]); // передача фамилии, имени, отчества  в функцию объединения.
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Lesson4-task3/Lesson4-task3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Lesson4_task3
+{
+    class Program
+    {
+        //Программа для определения времени года по номеру месяца
+        static void Main(string[] args)
+        {
+            startProg();
+        }
+        
+        static void startProg()
+        {
+            while(true){
+                
+                Console.WriteLine("Введите номер месяца.Для выхода нажмите 0.");
+                string input = Console.ReadLine();
+                int mounthNum = Convert.ToInt32(input);
+
+                //Проверка корректности ввода
+                if (mounthNum >= 1 && mounthNum<=12) {
+                    Console.WriteLine($"{seasons(mounthNum)}");// вызов функции и вывод результата
+                }
+                else
+                {
+                    Console.WriteLine("Некорректное значение месяца.");
+                }
+
+                //Выходим из программы 
+                if (input == "0") {
+                    return;
+                }
+               
+            }
+           
+            
+        }
+
+        static string seasons(int mounthNumb)
+        {
+            string season = "";
+
+            if (mounthNumb == 12 || mounthNumb == 1|| mounthNumb == 2)
+            {
+                 season = "Winter";
+            }
+            else if (mounthNumb == 3 || mounthNumb == 4 || mounthNumb == 5)
+            {
+                 season = "Spring";
+            }
+            else if(mounthNumb == 6 || mounthNumb == 7 || mounthNumb == 8) 
+            {
+                  season = "Summer";
+            }
+            else if(mounthNumb == 9 || mounthNumb == 10 || mounthNumb == 11)
+            {
+                  season = "Autumn";
+            }
+
+            return season;
+        }
+    }
+}
