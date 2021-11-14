d37749e13d02ba3c152bc33a035a7d82d9ec0cf1
Antonthik <93084395+Antonthik@users.noreply.github.com>
2021-11-14 22:09:11 +0300

    Add files via upload

---
+++ Lesson5task05/ToDo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5_task05
+{
+    class ToDo
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+
+        public ToDo (string title, bool isdone)
+        {
+            Title = title;
+            IsDone = isdone;
+        }
+
+        public void Getmetod()
+        {
+            Console.WriteLine($"Задача {this.IsDone} {this.Title}");
+        }
+
+    }
+}
